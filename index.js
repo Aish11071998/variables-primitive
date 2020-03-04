b45b27fea7580af0d9b934de8e9f514a75083426
Aish11071998 <aishwarya.sekar@ducenit.com>
2020-03-04 15:44:30 +0530

    1 file modified

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ primitive-string,boolean,numb,symbol
 reference-object,arrays
 special-NaN,null,undefined
 */
+//String Methods
 let a='Aishwarya';
 let b='Sekar'
 let ccat = a.concat(b);
@@ -45,3 +46,54 @@ console.log(tuc);
 let s='         Aishwarya Sekar     '
 let trim=s.trim();//removes spaces on both sides
 console.log(trim);
+
+//-----------------------------------------------------------------
+
+
+//Number Methods
+var num = 5.56789
+var n = num.toFixed(2);//rounds off the input after decimal 
+console.log(n);
+var n1=num.toExponential();//converts to exponential
+console.log(n1);
+var n2=num.toString();//converts to string
+console.log(n2);
+
+
+//-----------------------------------------------------------------
+//Global Methods
+//info,warn,error,log,table
+console.clear();//clears the console
+console.error('hi');//shows error symbol with the message given
+console.warn('Aishwarya');//shows warning symbol with message given
+console.log('Aishuuuuuuuuu');//shows the given message in console
+console.info('Sekar');//info is similar as log,just prints the                              output on the console 
+console.table(["Aish", "Meena", "Sekar"]);//table is created in console
+//alert('Aishwarya Meenakshi Sekar')//shows the alert box on the screen
+//var s=prompt("Aishwarya","Radhika");//displays the prompt message 
+                                         //on the screen
+var n= new Date();
+console.log(nn);//displays the num of  milliseconds
+var nn=Number(n);
+var n1="4567 5676";
+console.log(Number(n1));//output:NaN
+var x='4567';
+console.log(isNaN(x))// output:false
+var b = parseFloat("10.00")
+console.log(b)//output:10
+              //returns only the integer
+var c=parseInt("fasghj");//NaN
+console.log(c);      //returns an integer
+var a="Aishwarya";
+var l=a.length;
+console.log(l);//output:9
+              //returns length of the string
+var bool="Aishwarya";
+var b=Boolean(1>2);
+console.log(b)   //output:false
+                 //returns true if given condition is true or false           
+
+
+
+
+
